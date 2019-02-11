5510c8247c811e34c0a6aa27eb6af5a5a3b219f5
Nina <nina8duong@gmail.com>
2019-02-11 17:20:46 +0100

    Translate from norwegian to english on Course model

--- booking/models.py
+++ booking/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User, Group
 
 # Create your models here.
-class Emne(models.Model):
+class Subject(models.Model):
     title=models.CharField(max_length=50, unique=True)
-    course_code=models.CharField(max_length=10, unique=True)
-    studenter=models.ManyToManyField(User,limit_choices_to={'groups__name': "student"},related_name="studenter",blank=True)
-    studasser=models.ManyToManyField(User,limit_choices_to={'groups__name': "studass"},related_name="studasser",blank=True)
-    emneansvarlig = models.ForeignKey(User,limit_choices_to={'groups__name': "emne_ansvarlig"}, default = 0, on_delete=models.CASCADE)
+    subject_code=models.CharField(max_length=10, unique=True)
+    students=models.ManyToManyField(User, limit_choices_to={'groups__name': "student"}, related_name="studenter",blank=True)
+    assistant=models.ManyToManyField(User,limit_choices_to={'groups__name': "studass"}, related_name="studasser",blank=True)
+    subject_coordinator = models.ForeignKey(User, limit_choices_to={'groups__name': "emne_ansvarlig"}, default = None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
