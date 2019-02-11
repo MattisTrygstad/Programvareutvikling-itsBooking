41a3e2380a0f51cdadd0c7a76a8d63e43be7757d
Jonatan S. Engstad <jonaengs@gmail.com>
2019-02-11 20:22:10 +0100

    improve model translation, code structure and readability. Make populate_db code consistent with model

--- booking/admin.py
+++ booking/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Emne
-# Register your models here.
-admin.site.register(Emne)
+from .models import Course
+
+admin.site.register(Course)
--- booking/models.py
+++ booking/models.py
@@ -1,13 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 
-# Create your models here.
-class Subject(models.Model):
-    title=models.CharField(max_length=50, unique=True)
-    subject_code=models.CharField(max_length=10, unique=True)
-    students=models.ManyToManyField(User, limit_choices_to={'groups__name': "student"}, related_name="studenter",blank=True)
-    assistant=models.ManyToManyField(User,limit_choices_to={'groups__name': "studass"}, related_name="studasser",blank=True)
-    subject_coordinator = models.ForeignKey(User, limit_choices_to={'groups__name': "emne_ansvarlig"}, default = None, on_delete=models.CASCADE)
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+    subject_code = models.CharField(
+        max_length=10,
+        unique=True,
+    )
+    students = models.ManyToManyField(
+        User,
+        limit_choices_to={'groups__name': "students"},
+        related_name="courses_enrolled_in",
+        blank=True,
+    )
+    assistant = models.ManyToManyField(
+        User,
+        limit_choices_to={'groups__name': "assistants"},
+        related_name="courses_assisting_in",
+        blank=True,
+    )
+    course_coordinator = models.OneToOneField(
+        User,
+        limit_choices_to={'groups__name': "course_coordinators"},
+        default=None,
+        on_delete=models.CASCADE,
+        related_name="course"
+    )
 
     def __str__(self):
         return self.title
