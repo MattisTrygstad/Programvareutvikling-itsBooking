870ae74df2e361835b5bbe6837c086089032ad9a
Nina <nina8duong@gmail.com>
2019-02-11 16:59:06 +0100

    Create model 'Emne'

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
+class Emne(models.Model):
+    title=models.CharField(max_length=50, unique=True)
+    course_code=models.CharField(max_length=10, unique=True)
+    studenter=models.ManyToManyField(User,limit_choices_to={'groups__name': "student"},related_name="studenter",blank=True)
+    studasser=models.ManyToManyField(User,limit_choices_to={'groups__name': "studass"},related_name="studasser",blank=True)
+    emneansvarlig = models.ForeignKey(User,limit_choices_to={'groups__name': "emne_ansvarlig"}, default = 0, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
