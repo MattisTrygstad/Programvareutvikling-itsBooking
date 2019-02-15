812831614d378cd45ab21179f044dd3d898acdc9
Jonatan S. Engstad <jonaengs@gmail.com>
2019-02-15 16:40:34 +0100

    setup templates, view and helper for basic booking table rendering

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,5 @@
+import calendar
+
 from django.views.generic import DetailView
 
 from booking.models import Course
@@ -5,4 +7,9 @@ from booking.models import Course
 
 class CourseDetail(DetailView):
     model = Course
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['weekdays'] = list(calendar.day_name)[0:5]
+        return context
 
