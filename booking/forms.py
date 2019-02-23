2929b0831feff2e9c2fb1e0fd2ef2ffd445a8014
Jonatan S. Engstad <jonaengs@gmail.com>
2019-02-23 02:22:51 +0100

    add form clean method, fix modal info

--- booking/forms.py
+++ booking/forms.py
@@ -1,6 +1,17 @@
 from django import forms
+from django.contrib.auth.models import User
+
+from booking.models import BookingInterval
 
 
 class ReservationForm(forms.Form):
     reservation_index = forms.IntegerField(widget=forms.HiddenInput())
     booking_interval_nk = forms.CharField(widget=forms.HiddenInput())
+
+    def clean(self):
+        index = self.cleaned_data['reservation_index']
+        bi = BookingInterval.objects.get(nk=self.cleaned_data['booking_interval_nk'])
+        reserved_assistants = User.objects.filter(bookings__index=index)
+        bi_assistants = bi.assistants.all()
+        available_assistants = bi_assistants.difference(reserved_assistants)  # all assistants minus reserved ones
+        assert available_assistants.count() > 0, 'No assistants available for this reservation interval'
