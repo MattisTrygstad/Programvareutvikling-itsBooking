3da3484f5e1026445c046ddc8c26b258b1b35c5d
Jonatan S. Engstad <jonaengs@gmail.com>
2019-03-06 20:52:23 +0100

    fix file upload

--- assignments/views.py
+++ assignments/views.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView
 
 from assignments.models import Exercise
+from booking.models import Course
 
 
 class UploadExercise(SuccessMessageMixin, CreateView):
@@ -16,4 +18,11 @@ class UploadExercise(SuccessMessageMixin, CreateView):
         form = super().get_form()
         form.fields['file'].widget.attrs['onchange'] = 'display_filename(this)'
         return form
+
+    def form_valid(self, form):
+        exercise = form.save(commit=False)
+        exercise.student = self.request.user
+        exercise.course = get_object_or_404(Course, slug=self.kwargs['slug'])
+        exercise.save()
+        return HttpResponseRedirect(self.success_url)
 
