9526e8caf4eb31e7370c99f3124abbde47522051
Jonatan S. Engstad <jonaengs@gmail.com>
2019-03-07 14:07:45 +0100

    fix tests

--- assignments/tests.py
+++ assignments/tests.py
@@ -1,9 +1,10 @@
 import tempfile
 
-from django.contrib.auth.models import User, Group
-from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.test import TestCase
 from django.urls import reverse, reverse_lazy
 
+from assignments.models import Exercise
 from booking.models import Course
 
 
@@ -17,11 +18,21 @@ class ReservationTest(TestCase):
         self.client.login(username=self.username, password=self.password)
 
     def test_file_upload(self):
+        prev_count = Exercise.objects.count()
+        with open('manage.py') as f:
+            response = self.client.post(
+                reverse('upload_exercise', kwargs={'slug': self.course.slug}),
+                {'file': f}
+            )
+            self.assertEqual(302, response.status_code)
+            self.assertEqual(prev_count + 1, Exercise.objects.count())
+
+    def test_post_upload_no_file_fail(self):
+        prev_count = Exercise.objects.count()
         response = self.client.post(
-            reverse('upload_exercise', kwargs={'slug': self.course.slug}),
-            {'file': list(tempfile.TemporaryFile())}
+            reverse('upload_exercise', kwargs={'slug': self.course.slug})
         )
-        self.assertEqual(200, response.status_code)
+        self.assertFormError(response, 'form', 'file', 'Feltet er påkrevet.')
 
     def test_get_upload_page(self):
         response = self.client.get(reverse('upload_exercise', kwargs={'slug': self.course.slug}))
--- assignments/views.py
+++ assignments/views.py
@@ -1,7 +1,5 @@
-from django import forms
 from django.contrib.messages.views import SuccessMessageMixin
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView
 
 from assignments.models import Exercise
@@ -12,7 +10,7 @@ class UploadExercise(SuccessMessageMixin, CreateView):
     model = Exercise
     fields = ('file', )
     success_url = '/'
-    success_message = 'Exercise file %(file)s successfully uploaded to %(course)s!'
+    success_message = '%(file)s successfully uploaded to %(course)s!'
 
     def get_form(self):
         form = super().get_form()
@@ -24,5 +22,6 @@ class UploadExercise(SuccessMessageMixin, CreateView):
         exercise.student = self.request.user
         exercise.course = get_object_or_404(Course, slug=self.kwargs['slug'])
         exercise.save()
-        return HttpResponseRedirect(self.success_url)
-
+        # needed for the success message
+        form.cleaned_data['course'] = exercise.course
+        return super().form_valid(form)
