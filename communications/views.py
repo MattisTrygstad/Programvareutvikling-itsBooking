d571bffb919359e743776dbdc75e3b19d5bc2096
MattisTrygstad <mattislt@sf-nett.no>
2019-03-18 16:20:41 +0100

    indexView for announcements

--- communications/views.py
+++ communications/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'communications/announcements.html'
+    context_object_name = 'announcement_list'
+
+    def get_queryset(self):
+        """Return the last five published announcements"""
+        return Announcement.objects.filter(pub_date__lte=timezone.now()
+                ).order_by('date_published')
