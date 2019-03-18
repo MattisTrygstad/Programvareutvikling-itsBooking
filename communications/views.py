41e789783909d37716d0134a12e2a1cf69982bdd
MattisTrygstad <mattislt@sf-nett.no>
2019-03-18 21:24:09 +0100

    CreateView and AnnouncementForm

--- communications/views.py
+++ communications/views.py
@@ -1,15 +1,45 @@
 from django.shortcuts import render
 from communications.models import Announcement
 from django.utils import timezone
-from django.views.generic.list import ListView
+from django.views.generic import CreateView, ListView, UpdateView, TemplateView
 from django.views import generic
 from django.contrib.auth.models import User
 from django.db import models
+from .forms import AnnouncementForm
+from django.views.generic.edit import FormView
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.mixins import UserPassesTestMixin
 
-class IndexView(generic.ListView):
+class IndexView(TemplateView):
     template_name = 'communications/announcements.html'
-    context_object_name = 'announcement_list'
 
-    def get_queryset(self):
-        """Return the last five published announcements"""
-        return Announcement.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        announcements = Announcement.objects.all()
+        context.update({'announcements': announcements,
+                        'form': AnnouncementForm()})
+        return context
+    
+
+
+    def post(self, request, *args, **kwargs):
+        announcement_pk = self.request.POST['announcement_pk']
+        # handle reviews through a separate view
+        return CreateView.as_view()(request, *args, **kwargs, pk=announcement_pk)
+
+class CreateView(UpdateView):
+    template_name = 'communications/announcements.html'
+    model = Announcement
+    form_class = AnnouncementForm
+    
+    def get_success_url(self):
+        return HttpResponseRedirect(
+            reverse('announcements', kwargs={'slug': self.kwargs['slug']})
+        )
+
+    def form_valid(self, form):
+        announcement = form.save(commit=False)
+        announcement.author = self.request.user
+        announcement.save()
+        return self.get_success_url()
