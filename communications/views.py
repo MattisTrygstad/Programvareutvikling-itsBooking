24cf19a8456223c27a0afea500119de046fd74db
Jonatan S. Engstad <jonaengs@gmail.com>
2019-03-20 19:12:57 +0100

    setup basic announcement viewing for assistants

--- communications/views.py
+++ communications/views.py
@@ -1,27 +1,38 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
-from django.views.generic import CreateView, TemplateView, ListView
+from django.views.generic import CreateView, ListView
 
 from booking.models import Course
 from communications.models import Announcement
+from itsBooking.templatetags.helpers import user_in_group
 from .forms import AnnouncementForm
 
 
-class AnnouncementListView(ListView):
+class AnnouncementListView(UserPassesTestMixin, ListView):
     model = Announcement
+
+    def get_queryset(self):
+        course = get_object_or_404(Course, slug=self.kwargs['slug'])
+        return Announcement.objects.filter(course=course)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context.update({
-            'form': AnnouncementForm()
-        })
+        if user_in_group(self.request.user, "course_coordinators"):
+            context.update({
+                'form': AnnouncementForm()
+            })
         return context
 
     def post(self, request, *args, **kwargs):
         # handle reviews through a separate view
         return CreateAnnouncementView.as_view()(request, *args, **kwargs)
+
+    def test_func(self):
+        user = self.request.user
+        return user_in_group(user, 'course_coordinators') or user_in_group(user, 'assistants')
 
 
 class CreateAnnouncementView(CreateView):
