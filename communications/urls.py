0d7be5b8aba3b7a32307a5df506748289c5a4864
Jonatan S. Engstad <jonaengs@gmail.com>
2019-03-20 18:33:21 +0100

    fix bug, show profile pic on announcements

--- communications/urls.py
+++ communications/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from django.views.generic import ListView
 
-from communications.models import Announcement
 from communications.views import *
 
 urlpatterns = [
-    path('<str:slug>/announcements/', AnnouncementView.as_view(), name='announcements'),
+    path('<str:slug>/announcements/', AnnouncementListView.as_view(), name='announcements'),
 ]
--- communications/views.py
+++ communications/views.py
@@ -1,27 +1,31 @@
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView
+
+from booking.models import Course
 from communications.models import Announcement
 from .forms import AnnouncementForm
 
 
-class AnnouncementView(TemplateView):
-    template_name = 'communications/announcements.html'
+class AnnouncementListView(ListView):
+    model = Announcement
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        announcements = Announcement.objects.all()
-        context.update({'announcements': announcements,
-                        'form': AnnouncementForm()})
+        context.update({
+            'form': AnnouncementForm()
+        })
         return context
 
     def post(self, request, *args, **kwargs):
         # handle reviews through a separate view
         return CreateAnnouncementView.as_view()(request, *args, **kwargs)
 
+
 class CreateAnnouncementView(CreateView):
-    template_name = 'communications/announcements.html'
+    template_name = 'communications/announcement_list.html'
     model = Announcement
     form_class = AnnouncementForm
 
@@ -34,5 +38,9 @@ class CreateAnnouncementView(CreateView):
         announcement = form.save(commit=False)
         announcement.author = self.request.user
         announcement.timestamp = timezone.now()
+        announcement.course = get_object_or_404(
+            Course,
+            slug=self.kwargs['slug']
+        )
         announcement.save()
         return self.get_success_url()
